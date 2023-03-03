14c4d2b9563c8c7894ed55e2499c5dd8f675ef92
farhanpermana <46885640+farhanpermana@users.noreply.github.com>
2023-03-03 08:30:21 +0700

    added room models

---
+++ HotelsProject/Models/Room.swift
@@ -0,0 +1,14 @@
+//
+//  Room.swift
+//  HotelsProject
+//
+//  Created by Phinconers on 03/03/23.
+//
+
+import Foundation
+
+struct Room {
+    var roomImg: String
+    var roomTitle: String
+    var roomDesc: String
+}
